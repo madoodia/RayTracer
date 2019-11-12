41dcbde9857c39f72c2b195c046fea5d443ed9c2
Mahmoodreza Aarabi <madoodia@gmail.com>
2019-11-11 18:35:55 -0800

    04 perlinNoise 2

--- src/perlin.h
+++ src/perlin.h
@@ -15,6 +15,19 @@
 
 #include "vec3.h"
 
+inline float trilinearInterp(float c[2][2][2], float u, float v, float w)
+{
+    float accum = 0;
+    for (int i = 0; i < 2; i++)
+        for (int j = 0; j < 2; j++)
+            for (int k = 0; k < 2; k++)
+                accum += (i * u + (1 - i) * (1 - u)) *
+                         (j * v + (1 - j) * (1 - v)) *
+                         (k * w + (1 - k) * (1 - w)) * c[i][j][k];
+
+    return accum;
+}
+
 class Perlin
 {
 public:
@@ -26,7 +39,15 @@ public:
         int i = floor(p.x);
         int j = floor(p.y);
         int k = floor(p.z);
-        return ranfloat[(permX[i] & 255) ^ (permY[j] & 255) ^ (permZ[k] & 255)];
+
+        float c[2][2][2];
+        for (int di = 0; di < 2; di++)
+            for (int dj = 0; dj < 2; dj++)
+                for (int dk = 0; dk < 2; dk++)
+                    c[di][dj][dk] = ranfloat[permX[(i + di) & 255] ^
+                                             permY[(j + dj) & 255] ^
+                                             permZ[(k + dk) & 255]];
+        return trilinearInterp(c, u, v, w);
     }
 
     static float *ranfloat;
